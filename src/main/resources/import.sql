cb6a0ceb7dff3fc1d9167644d9678e1068f7b3f9
Abdilasis Abdallah <abdilasis09@gmail.com>
2018-06-21 11:14:38 +0100

    first commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO Book (id, bookTitle, genre, publishYear) VALUES (1, 'myBook', 'Horror', '2015');
+INSERT INTO Book (id, bookTitle, genre, publishYear) VALUES (2, 'myBook2', 'Adventure', '2016');
+INSERT INTO Book (id, bookTitle, genre, publishYear) VALUES (3, 'myBook3', 'Drama', '2017'); 
